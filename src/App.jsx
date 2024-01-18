642ccf755f36ef9ae64d1a8a5cca0fa282c512ba
Marek-Kosmowski <kosmowski.marek@gmail.com>
2024-01-18 20:50:57 +0100

    Manage state to switch between components

--- src/App.jsx
+++ src/App.jsx
@@ -4,21 +4,40 @@ import DefaultBackground from './components/DefaultBackground';
 import { useState } from 'react';
 
 function App() {
-  const [newProject, setNewProject] = useState(false);
+  const [newProject, setNewProject] = useState({
+    projectId: undefined,
+    projects: [],
+  });
 
-  const handleProjectButton = () => {
-    setNewProject(true);
+  const handleNewProjectButton = () => {
+    setNewProject((prevState) => {
+      return {
+        ...prevState,
+        projectId: null,
+      };
+    });
   };
+
+  let content;
+
+  console.log(newProject.projectId);
+
+  if (newProject.projectId === null) {
+    content = <NewProject />;
+  } else if (newProject.projectId === undefined) {
+    content = <DefaultBackground onChange={handleNewProjectButton} />;
+  }
 
   return (
     <main className='h-screen my-8 flex gap-10'>
-      <ProjectsSidebar onChange={handleProjectButton} />
+      <ProjectsSidebar onChange={handleNewProjectButton} />
 
-      {!newProject ? (
-        <DefaultBackground onChange={handleProjectButton} />
+      {/* {!newProject ? (
+        <DefaultBackground onChange={handleNewProjectButton} />
       ) : (
         <NewProject onChange={() => setNewProject(false)} />
-      )}
+      )} */}
+      {content}
     </main>
   );
 }
