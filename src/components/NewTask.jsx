868785d0307dadb971a5f9b6cee61b6a1a685c07
Marek-Kosmowski <kosmowski.marek@gmail.com>
2024-01-27 23:06:04 +0100

    Add modal for invalid input inside NewTask component

--- src/components/NewTask.jsx
+++ src/components/NewTask.jsx
@@ -1,14 +1,23 @@
 import { useState } from 'react';
-
+import { useRef } from 'react';
+import Modal from './Modal';
 export default function NewTask({ onCreate }) {
   const [inputTask, setInpuTask] = useState('');
 
+  const modal = useRef();
+  const newInputTask = useRef();
   const handleChange = (e) => {
     setInpuTask(e.target.value);
   };
 
   const handleClickBtn = () => {
+    const enteredInputTask = newInputTask.current.value;
     setInpuTask('');
+
+    if (enteredInputTask.trim() === '') {
+      modal.current.open();
+      return;
+    }
 
     onCreate({
       task: inputTask,
@@ -16,16 +25,23 @@ export default function NewTask({ onCreate }) {
   };
 
   return (
-    <div className='flex gap-4 mb-6'>
-      <input
-        onChange={handleChange}
-        className='w-60 bg-gray-200 py-1 px-2 rounded focus:outline-none'
-        type='text'
-        value={inputTask}
-      />
-      <button onClick={handleClickBtn} className='font-medium text-gray-700'>
-        Add Task
-      </button>
-    </div>
+    <>
+      <Modal ref={modal}>
+        <h2 className='mb-2  font-bold text-xl text-gray-700'>Invalid input</h2>
+        <p className='mb-3'>Please provide a value for a task input!</p>
+      </Modal>
+      <div className='flex gap-4 mb-6'>
+        <input
+          ref={newInputTask}
+          onChange={handleChange}
+          className='w-60 bg-gray-200 py-1 px-2 rounded focus:outline-none'
+          type='text'
+          value={inputTask}
+        />
+        <button onClick={handleClickBtn} className='font-medium text-gray-700'>
+          Add Task
+        </button>
+      </div>
+    </>
   );
 }
